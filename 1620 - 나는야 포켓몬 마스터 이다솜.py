b663e2ffdd059b4b3d2b0838ec5d2a8b4c08301c
MinChoi0129 <64841576+MinChoi0129@users.noreply.github.com>
2020-11-26 21:39:37 +0900

    Add files via upload

--- 1620 - 나는야 포켓몬 마스터 이다솜.py
+++ 1620 - 나는야 포켓몬 마스터 이다솜.py
@@ -3,10 +3,17 @@ import sys
 n, m = map(int, sys.stdin.readline().rstrip().split())
 
 pokemon = dict()
+pokemon_l = []
 
-for i in range(1, n):
-    pokemon[str(i)] = sys.stdin.readline().rstrip()
+for i in range(1, n + 1):
+    name = sys.stdin.readline().rstrip()
+    pokemon[name] = i
+    pokemon_l.append(name)
 
 for i in range(m):
     question = sys.stdin.readline().rstrip()
     
+    if question.isdigit():
+        print(pokemon_l[int(question) - 1])
+    else:
+        print(pokemon[question])
