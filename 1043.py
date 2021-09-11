492622a0e021a71726b054cfaa406bb2146cf101
MinChoi0129 <wjdchs0129@naver.com>
2021-09-11 11:45:53 +0900

    Update 1043.py
    수정 필요
    틀림

--- 1043.py
+++ 1043.py
@@ -1,16 +1,30 @@
 n, m = map(int, input().split())
 
-how_many_know, their_numbers = 0, []
-how_many_know, *their_numbers = map(int, input().split())
+their_numbers = map(int, input().split())[1:]
 
 knows = [False] * (n+1) # 0번 인덱스 무시
+all_parties = [] # 파티 기록
 
 for number in their_numbers: # 초기 확진자
-    print(number)
     knows[number] = True
 
 for _ in range(m):
-    input()
+    party = list(map(int, input().split()))[1:] # 개별 파티
+    all_parties.append(party)
+    for i in range(len(party)):
+        if knows[party[i]]: # 확진자가 그 파티에 있으면
+            for j in range(len(party)): # 모든 파티 참여자는 감염
+                knows[party[j]] = True
+            break
 
-print(how_many_know, their_numbers)
-print(knows)
+count = 0
+for party in all_parties:
+    canGo = True
+    for human in party:
+        if knows[human]:
+            canGo = False
+            break
+    if canGo:
+        count += 1
+
+print(count)
