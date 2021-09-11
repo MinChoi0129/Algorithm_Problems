49fe6974d6a769e1ae9f27be2bf42cf46e4b9246
MinChoi0129 <wjdchs0129@naver.com>
2021-09-11 20:28:08 +0900

    1043 맞춤!!

--- 1043.py
+++ 1043.py
@@ -1,27 +1,31 @@
 n, m = map(int, input().split())
 
-their_numbers = map(int, input().split())[1:]
+how_many_know, their_numbers = 0, []
+how_many_know, *their_numbers = map(int, input().split())
 
-knows = [False] * (n+1) # 0번 인덱스 무시
+knows = set()
 all_parties = [] # 파티 기록
 
 for number in their_numbers: # 초기 확진자
-    knows[number] = True
+    knows.add(number)
 
 for _ in range(m):
     party = list(map(int, input().split()))[1:] # 개별 파티
     all_parties.append(party)
-    for i in range(len(party)):
-        if knows[party[i]]: # 확진자가 그 파티에 있으면
-            for j in range(len(party)): # 모든 파티 참여자는 감염
-                knows[party[j]] = True
-            break
+
+for _ in range(m):
+    for party in all_parties:
+        for human in list(knows):
+            if human in party:
+                for smallhuman in party:
+                    knows.add(smallhuman)
+                
 
 count = 0
 for party in all_parties:
     canGo = True
     for human in party:
-        if knows[human]:
+        if human in list(knows):
             canGo = False
             break
     if canGo:
