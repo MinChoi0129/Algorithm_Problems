cf76160f5c34ca7509c6b5dcdd124bba7e18790f
MinChoi0129 <wjdchs0129@naver.com>
2021-09-19 14:15:56 +0900

    Update 3273.py

--- 3273.py
+++ 3273.py
@@ -1,18 +1,16 @@
 n = int(input())
 seq = list(map(int, input().split()))
+seq.sort()
 x = int(input())
 cnt = 0
-
 p1, p2 = 0, n - 1
 
-while True:
-    if seq[p1] + seq[p2] == x:
+while p1 < p2:
+    tmp = seq[p1] + seq[p2]
+    if tmp == x:
         cnt += 1
-    p1 += 1
-
-    if p1 == p2:
-        p1 = 0
+    if tmp < x:
+        p1 += 1
+    else:
         p2 -= 1
-        if p2 == 0:
-            break
 print(cnt)
