9499edc75d0481473abd0c679fb2869b3afeb2ad
MinChoi0129 <wjdchs0129@naver.com>
2021-09-26 14:16:15 +0900

    solved

--- 16236.py
+++ 16236.py
@@ -1,5 +1,4 @@
 from collections import deque
-from copy import deepcopy
 
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
@@ -9,112 +8,68 @@ class BabyShark:
         self.size = 2
         self.timer = 0
         self.position = position
+        self.eatCount = 0
         
     def move(self, to, time):
         global board
-        board[self.position[0]][self.position[1]] = 0
-        self.position = to
         self.timer += time
-        self.size += board[to[0]][to[1]]
-        board[to[0]][to[1]] = 0
+        board[self.position[0]][self.position[1]] = 0 # 이동 전 위치 물고기 먹음
+        self.position = to # 이동 완료
+        self.eatCount += 1
+        if self.eatCount == self.size:
+            self.size += 1
+            self.eatCount = 0
+        board[to[0]][to[1]] = 0 # 이동 후 위치 물고기 먹음
     
-    def getClosestFish(self): # 여러좌표를 리턴할 수도, -1을 리턴할수도 있음
-        global board, n
-        edibleFishes = [] # 보이는 먹을 수 있는 물고기 그냥 다 담음
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] in [1, 2, 3, 4, 5, 6] and board[i][j] < self.size:
-                    edibleFishes.append([i, j])
-        
-        
-        
-        
-        minDistance = 1e9
-        closestFishes = []
-        for fishPos in edibleFishes: # 보이는 물고기들 중에서
-            d = self.나와물고기사이의astar거리(fishPos) # 어느 한마리 붙잡고 거리측정
-            if minDistance > d:
-                closestFishes.clear()
-                minDistance = d
-                closestFishes.append(fishPos)
-            elif minDistance == d:
-                closestFishes.append(fishPos)
-            else:
-                continue
-        
-        #결국 closestFishes에는 최단거리 1마리 이상의 좌표가 담김
-            
-            
-        closestFishes.sort(key = lambda x : (x[0], x[1]))
-        return [closestFishes[0], minDistance] if len(closestFishes) > 0 else -1
-    
-    def 나와물고기사이의astar거리(self, fishPos):
-        global board, n
-        start = deepcopy(self.position)
-        goal = fishPos
-        distanceCalculator = 0
-        dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
-        #goal이 아예 inapprochable한지
-        
-        apprachable = False
-        for xy in dxy:
-            newX, newY = goal[0] + xy[0], goal[1] + xy[1]
-            if not (0 <= newX < n and 0 <= newY < n):
-                continue
-            
-            # 여기는 보드 안
-            newPoint = board[newX][newY]
-            if newPoint <= self.size:
-                apprachable = True
-                break
-            
-        if not apprachable:
-            return -1
-        
-        visited = [[False]*n for _ in range(n)]
-        visited[self.position[0]][self.position[1]] = True
-        
-        while True:
-            if start == goal:
-                return distanceCalculator
-            
-            effF = 1e9
-            effX, effY = -1, -1
-            for xy in dxy:
-                newX, newY = start[0] + xy[0], start[1] + xy[1]
-                
-                if not (0 <= newX < n and 0 <= newY < n):
-                    continue
-                
-                if board[newX][newY] > self.size and board[newX][newY] != 9:
-                    continue
-                
-                if visited[newX][newY]:
-                    continue
-                visited[newX][newY] = True
-                g = abs(newX - start[0]) + abs(newY - start[1])
-                h = abs(newX - goal[0]) + abs(newY - goal[1])
-                f = g + h
-                if effF > f:
-                    effF, effX, effY = f, newX, newY
-            start[0], start[1] = effX, effY
-            distanceCalculator += 1
-    
+    def bfs(self):
+        global board, n 
+        dxy = [ (-1, 0), (1, 0), (0, -1), (0, 1) ]
+        q = deque([[self.position[0], self.position[1]]])
+        visited = [[0] * n for _ in range(n)]
+        visited[self.position[0]][self.position[1]] = 1
+        approachableFishes = []
 
+        while q:
+            x, y = q.popleft()
+            for dx, dy in dxy:
+                newX, newY = x + dx, y + dy
+                
+                if newX < 0 or newX >= n or newY < 0 or newY >= n:
+                    continue
+                if visited[newX][newY] > 0 or board[newX][newY] > self.size:
+                    continue
+                
+                visited[newX][newY] = visited[x][y] + 1
+                
+                if board[newX][newY] != 0 and board[newX][newY] < self.size:
+                    approachableFishes.append([newX, newY])
+                    
+                q.append([newX, newY])
+
+        closestDistance = 1e9
+        closestFishes = []
+        for fishPos in approachableFishes:
+            if visited[fishPos[0]][fishPos[1]] <= closestDistance:
+                closestDistance = visited[fishPos[0]][fishPos[1]]
+                closestFishes.append(fishPos)
+        
+        closestFishes.sort(key = lambda x : (x[0], x[1]))
+            
+        return [closestFishes[0], closestDistance - 1] if closestFishes else -1
+    
 '''초기화'''
 startPostition = [-1, -1]
 for i in range(n):
     for j in range(n):
         if board[i][j] == 9:
+            board[i][j] = 0
             startPostition = [i, j]
 
 shark = BabyShark(startPostition)
 
-
 '''동작'''
 while True:
-    fishes = shark.getClosestFish() # 여러개 들어올 수도 있음
+    fishes = shark.bfs()
     if fishes == -1:
         print(shark.timer)
         break
