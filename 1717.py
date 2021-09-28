9513c3529e78776493b9d20cbcd324abda2f8db1
MinChoi0129 <wjdchs0129@naver.com>
2021-09-28 22:35:09 +0900

    Update 1717.py

--- 1717.py
+++ 1717.py
@@ -1,36 +1,34 @@
 import sys
+sys.setrecursionlimit(1000000)
 input = lambda : sys.stdin.readline().rstrip()
 
 n, m = map(int, input().split())
-sets = []
+parents = dict()
 for i in range(n + 1):
-    tmp = set()
-    tmp.add(i)
-    sets.append(tmp)
+    parents[i] = i
+
+
+def findParent(parents, x):
+    if parents[x] != x: # Root Node가 아니라면
+        parents[x] = findParent(parents, parents[x])
+        return parents[x]
+    return x
+
+def unionParents(parents, a, b):
+    a = findParent(parents, a)
+    b = findParent(parents, b)
+
+    if a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
 
 for _ in range(m):
-    op, a, b = input().split()
-    if op == '0':
-        tmp = set()
-        for i in range(len(sets)):
-            if int(a) in sets[i]:
-                tmp.update(sets[i])
-                sets[i] = set()
-            if int(b) in sets[i]:
-                tmp.update(sets[i])
-                sets[i] = set()
-        sets.append(tmp)
-        while True:
-            if set() not in sets:
-                break
-            sets.remove(set())
-        print(sets)
-        
-                
-    elif op == '1':
-        find = False
-        for s in sets:
-            if int(a) in s and int(b) in s:
-                find = True
-                break
-        print("YES" if find else "NO")
+    op, a, b = map(int, input().split())
+    if op == 0:
+        unionParents(parents, a, b)
+    elif op == 1:
+        if findParent(parents, a) == findParent(parents, b):
+            print("YES")
+        else:
+            print("NO")
