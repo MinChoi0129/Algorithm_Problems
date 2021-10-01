38418b5f1e858d8e6c42163c2fbdee2c4f1d1638
MinChoi0129 <wjdchs0129@naver.com>
2021-10-01 22:18:19 +0900

    solved

--- 17214.py
+++ 17214.py
@@ -13,24 +13,39 @@ while i < len(polynomial):
         tmp += polynomial[i]
         i += 1
 statement.append(tmp)
+if '' in statement:
+    statement.pop(0)
+
+print(statement)
 
 for i in statement:
     if i == '+' or i == '-':
         print(i, end = "")
         continue
-
-    xCount = 0
-    for j in i[::-1]:
-        if j == 'x':
-            xCount += 1
-    coeff = i[:len(i) - xCount]
     
-    if xCount == 0:
-        print(coeff + 'x+W', end = "")
+    if i == '0':
+        print("W", end = "")
+    
+    else:
+        xCount = 0
+        for j in i:
+            if j == 'x':
+                xCount += 1
+        coEff = int(i[ : len(i) - xCount])
+                
+        if xCount == 0:
+            if coEff == 1:
+                print('x+W', end = "")
+            else:
+                print(str(coEff) + 'x+W')
         
-    elif xCount > 0:
-        newCoEff = int(coeff) // (xCount + 1)
-        if newCoEff > 1:
-            print(newCoEff, end = "")
-        for _ in range(xCount + 1):
-            print("x", end = "")
+        else:
+            integralCoEff = str(coEff // (xCount + 1))
+            if integralCoEff == '1':
+                print('x' * (xCount + 1), end = "")
+            else:
+                print(integralCoEff, end = "")
+                print('x' * (xCount + 1), end = "")
+
+if statement[-1] not in [str(i) for i in range(1, 10001)] and statement[-1] != '0':
+    print("+W", end = "")
