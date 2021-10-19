60174654de2fc6f7cb9935649ebc1d1d5f56c646
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-10-19 20:18:54 +0900

    Update 2174.py

--- 2174.py
+++ 2174.py
@@ -5,43 +5,43 @@ def getAllRobotsPos(my_robots):
     return [(my_robots[i].x, my_robots[i].y) for i in range(len(my_robots))]
 
 class Robot:
-    def __init__(self, x, y, direction) -> None:
+    def __init__(self, x, y, direction):
         self.x = x - 1
         self.y = y - 1
         self.direction = direction
     
     def moveORturn(self, mode, times):
-        if mode == "F":
+        if mode == "F": # move
             for _ in range(times):
                 if self.direction == 'N':
-                    for idx, pos in getAllRobotsPos(my_robots):
+                    for idx, pos in enumerate(getAllRobotsPos(my_robots)):
                         if (self.x, self.y + 1) == pos:
                             return idx
                     self.y += 1
                     if not (0 <= self.x < A and 0 <= self.y < B):
-                        return False
+                        raise IndexError
                 elif self.direction == 'S':
-                    for idx, pos in getAllRobotsPos(my_robots):
+                    for idx, pos in enumerate(getAllRobotsPos(my_robots)):
                         if (self.x, self.y - 1) == pos:
                             return idx
                     self.y -= 1
                     if not (0 <= self.x < A and 0 <= self.y < B):
-                        return False    
+                        raise IndexError   
                 elif self.direction == 'W':
-                    for idx, pos in getAllRobotsPos(my_robots):
+                    for idx, pos in enumerate(getAllRobotsPos(my_robots)):
                         if (self.x - 1, self.y) == pos:
                             return idx
                     self.x -= 1
                     if not (0 <= self.x < A and 0 <= self.y < B):
-                        return False
+                        raise IndexError
                 elif self.direction == 'E':
-                    for idx, pos in getAllRobotsPos(my_robots):
+                    for idx, pos in enumerate(getAllRobotsPos(my_robots)):
                         if (self.x + 1, self.y) == pos:
                             return idx
                     self.x += 1
                     if not (0 <= self.x < A and 0 <= self.y < B):
-                        return False
-        else:
+                        raise IndexError
+        else: # turn
             if mode == 'L':
                 for _ in range(times):
                     if self.direction == 'E':
@@ -74,11 +74,13 @@ for _ in range(N):
 
 commands = [input().split() for _ in range(M)]
 for cmd in commands:
-    result = my_robots[int(cmd[0])].moveORturn(cmd[1], int(cmd[2]))
-    if not result:
+    try:
+        result = my_robots[int(cmd[0])].moveORturn(cmd[1], int(cmd[2]))
+        if type(result) == int:
+            print("Robot", cmd[0], "crashes into robot", result)
+            sys.exit()
+    except IndexError:
         print("Robot", cmd[0], "crashes into the wall")
         sys.exit()
-    elif type(result) == int:
-        print("Robot", cmd[0], "crashes into robot", result)
-        sys.exit()
+        
 print("OK")
