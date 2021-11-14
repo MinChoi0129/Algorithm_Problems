b49971ba5a4db34496d821882b8e562719b55c0f
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-14 13:02:36 +0900

    Update 1038.py

--- 1038.py
+++ 1038.py
@@ -1,30 +1,42 @@
 n = int(input())
 
-descendingNumbers = []
+descendingNumbers, count = [0,1,2,3,4,5,6,7,8,9], 10
+is_descending = dict()
 
-def isDescending(num):
-    if len(num) == 1:
-        return True
+for i in range(10):
+    is_descending[str(i)] = True
     
-    else:
-        before = int(num[0])
-        for i in range(1, len(num)):
-            if int(num[i]) >= before:
-                return False
-        return True
-
-num = 0
+num = 10
 while True:
-    if isDescending(str(num)):
-        descendingNumbers.append(num)
-    if len(descendingNumbers) > n:
+    
+    if num > 987654321 or count >= n + 1:
         break
-    if num > 987654321:
-        break
+    
+    str_num = str(num)
+    if int(str_num[0]) <= int(str_num[1]): # 첫 두자리수부터 감소 안함
+        is_descending[str_num] = False
+    else: # 첫 두자리수는 감소
+        try:
+            if not is_descending[str_num[1:]]:
+                is_descending[str_num] = False
+            else:
+                is_descending[str_num] = True
+                descendingNumbers.append(num)
+                count += 1
+        except:
+            flag = True
+            for i in range(len(str_num) - 1):
+                if str_num[i] <= str_num[i+1]:
+                    flag = False
+                    break
+            
+            is_descending[str_num] = flag
+            
+            if flag:                
+                descendingNumbers.append(num)
+                count += 1
+
     num += 1
-
-
-if len(descendingNumbers) == n + 1:
-    print(descendingNumbers[n])
-else:
-    print(-1)
+    
+try: print(descendingNumbers[n])
+except: print(-1)
