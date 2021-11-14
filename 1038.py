d34909f99ddc23814d3dbeb9a90bc95bbec54bdf
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-14 22:15:25 +0900

    Update 1038.py

--- 1038.py
+++ 1038.py
@@ -1,13 +1,8 @@
 from itertools import combinations as C
-
-n = int(input())
-
 descending_numbers = []
 for i in range(1, 11):
     for combi in C([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], i):
         descending_numbers.append(int(''.join(map(str, sorted(combi, reverse=True)))))
-
 descending_numbers.sort()
-
-try: print(descending_numbers[n])
+try: print(descending_numbers[int(input())])
 except: print(-1)
