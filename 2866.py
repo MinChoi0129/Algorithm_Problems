d2bbe982e3b8f7e45f20d111216ae69a2a83682f
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-16 19:54:26 +0900

    .

--- 2866.py
+++ 2866.py
@@ -1,20 +1,20 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-r, c = map(int, input().split())
-count = 0
+# import sys
+# input = lambda : sys.stdin.readline().rstrip()
+# r, c = map(int, input().split())
+# count = 0
 
-board = [input() for _ in range(r)]
+# board = [input() for _ in range(r)]
 
-while count < r:
-    dictionary = dict()
-    for y in range(c):
-        tmp = ""
-        for x in range(count + 1, r): tmp += board[x][y]
-        if tmp in dictionary: dictionary[tmp] += 1
-        else: dictionary[tmp] = 1
+# while count < r:
+#     dictionary = dict()
+#     for y in range(c):
+#         tmp = ""
+#         for x in range(count + 1, r): tmp += board[x][y]
+#         if tmp in dictionary: dictionary[tmp] += 1
+#         else: dictionary[tmp] = 1
 
-    for val in dictionary.values():
-        if val != 1:
-            print(count)
-            sys.exit()
-    count += 1
+#     for val in dictionary.values():
+#         if val != 1:
+#             print(count)
+#             sys.exit()
+#     count += 1
