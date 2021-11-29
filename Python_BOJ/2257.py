b17a0475e4d4c1dc7e96b8491cfa7fa2b12a9d34
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-29 22:24:08 +0900

    Update 2257.py

--- Python_BOJ/2257.py
+++ Python_BOJ/2257.py
@@ -1,46 +1,14 @@
-MassTable = {'H' : 1, 'C' : 12, 'O' : 16}
-OtherTable = ['2', '3', '4', '5', '6', '7', '8', '9', '(', ')']
-chemicalStatement = input()
+mass_table, stack, chemical_statement = {'H':1, 'C':12, 'O':16}, [], input()
 
-
-def chemicalMassCalculator(statement): # H20 LEN = 3
-    chemicalMass = 0
-    i = 0
-    while i != len(statement):
-        try:
-            if statement[i] == '(':
-                tmpChemicalStatement = ""
-                while True:
-                    i += 1
-                    if statement[i] == ')':
-                        i += 1
-                        break
-                    else:
-                        tmpChemicalStatement += statement[i]
-                
-                multiplier = 1
-                try:
-                    if statement[i] in OtherTable[:8]:
-                        multiplier = int(statement[i])
-                except:
-                    pass
-                
-                chemicalMass += (multiplier * chemicalMassCalculator(tmpChemicalStatement))
-                
-            elif statement[i] not in OtherTable:
-                if statement[i+1] in OtherTable[:8]:
-                    chemicalMass += (MassTable[statement[i]] * int(statement[i+1]))
-                    i += 2
-                else:
-                    chemicalMass += MassTable[statement[i]]
-                    i += 1
-            else:
-                i += 1
-            
-        except:
-            chemicalMass += MassTable[statement[-1]]
-            i += 1
-            
-    return chemicalMass
-                    
-print(chemicalMassCalculator(chemicalStatement))
+for char in chemical_statement:
+    if char in mass_table: stack.append(mass_table[char]) # 원소기호
+    elif char == '(': stack.append(char) # 개괄호
+    elif char in "23456789": stack.append(stack.pop() * int(char)) # 숫자
+    elif char == ')': # 폐괄호
+        sum_in_bracket = 0
+        while True:
+            pop = stack.pop() # 5번줄과 연계, 정수형
+            if pop == '(': break
+            sum_in_bracket += pop
+        stack.append(sum_in_bracket)
+print(sum(stack))
