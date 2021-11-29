8338c5daf30bbeba38a7b2dce70d877b5eebee76
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-29 20:06:36 +0900

    .

--- Python_BOJ/1501.py
+++ Python_BOJ/1501.py
@@ -1,21 +1,24 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-def getTranslatableCount(word):
-    word = list(word)
-    first_end = word[0] + word[-1]
-    middle_word = ''.join(sorted(word[1:-1]))
-    return dictionary[first_end + middle_word]
+def getTranslatableCount(word : str):
+    if len(word) <= 2:
+        try: return dictionary[word]
+        except: return 0
+    else:
+        first_end = word[0] + word[-1]
+        middle_word = ''.join(sorted(word[1:-1]))
+        try: return dictionary[first_end + middle_word]
+        except: return 0
 
-n = int(input())
+# 사전 만들기
 dictionary = dict()
-
-for _ in range(n):
-    word = list(input())
+for _ in range(int(input())):
+    word = input()
     key = ""
     if len(word) <= 2:
         key = word
-    else:        
+    else:
         first_end = word[0] + word[-1]
         middle_word = ''.join(sorted(word[1:-1]))
         key = first_end + middle_word
@@ -23,15 +26,14 @@ for _ in range(n):
     if key not in dictionary: dictionary[key] = 1
     else: dictionary[key] += 1
         
+# 문장 해석
 for _ in range(int(input())):
-    if n == 0:
-        print(0)
-    else:
-        sentence = input().split()
-        result = 1
-        for word in sentence:
-            if len(word) <= 2:
-                result *= 1
-            else:
-                result *= getTranslatableCount(word)
-        print(result)
+    sentence = input().split()
+    result, possible = 1, False
+    
+    for word in sentence:
+        val = getTranslatableCount(word)
+        if val != 0:
+            result *= val; possible = True
+                   
+    print(result if possible else 0)
