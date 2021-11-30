68b4a4af4c199a10acfcb7a1889e350ffcb0009b
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2021-11-30 19:07:03 +0900

    .

--- Python_ETC/instagramChecker.py
+++ Python_ETC/instagramChecker.py
@@ -1,6 +1,6 @@
 import os
 followerFILE = open("follower.txt", mode = "r",  encoding="utf-8")
-followingFILE = open("following.txt", mode = "r",  encoding="utf-8")
+followingFILE = open("tst.txt", mode = "r",  encoding="utf-8")
 follower, following, j = [], [], 0
 os.system("cls")
 for i in followerFILE:
@@ -11,33 +11,36 @@ for i in followingFILE:
     if "님의 프로필 사진" in i:
         following.append(i[:-10])
 
-len_of_longest_letter = 0
-for i in follower:
-    if len(i) > len_of_longest_letter:
-        len_of_longest_letter = len(i)
-for i in following:
-    if len(i) > len_of_longest_letter:
-        len_of_longest_letter = len(i)
+for human in follower:
+    if human not in following:
+        print(human)
+# len_of_longest_letter = 0
+# for i in follower:
+#     if len(i) > len_of_longest_letter:
+#         len_of_longest_letter = len(i)
+# for i in following:
+#     if len(i) > len_of_longest_letter:
+#         len_of_longest_letter = len(i)
 
 
-print("팔로워 수: ", len(follower))
-print("팔로잉 수: ", len(following))
+# print("팔로워 수: ", len(follower))
+# print("팔로잉 수: ", len(following))
 
-print("\n내가 팔로우 하지만 상대는 안하는 경우  : ")
-for i in following:
-    if i not in follower:
-        print("%25s" %(i), end = " | ")
-        j += 1
-        if j == 4:
-            print()
-            j =  0
+# print("\n내가 팔로우 하지만 상대는 안하는 경우  : ")
+# for i in following:
+#     if i not in follower:
+#         print("%25s" %(i), end = " | ")
+#         j += 1
+#         if j == 4:
+#             print()
+#             j =  0
 
-j = 0
-print("\n나는 팔로우 안하지만 상대가 하는 경우  : ")
-for  i in follower:
-    if i not in following:
-        print("%25s" %(i), end = " | ")
-        j += 1
-        if j == 4:
-            print()
-            j =  0
+# j = 0
+# print("\n나는 팔로우 안하지만 상대가 하는 경우  : ")
+# for  i in follower:
+#     if i not in following:
+#         print("%25s" %(i), end = " | ")
+#         j += 1
+#         if j == 4:
+#             print()
+#             j =  0
