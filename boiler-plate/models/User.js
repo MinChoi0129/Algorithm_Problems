d264c1190e713abbcff11f61627a22542ded0711
MinJae Lee <wjdchs0129@gnu.ac.kr>
2021-12-06 22:44:02 +0900

    Update User.js

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const { is } = require('type-is')
-const saltRounds = 10
 const jwt = require('jsonwebtoken')
-
-
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -36,41 +33,50 @@ const userSchema = mongoose.Schema({
     }
 })
 
-
-userSchema.pre('save', function(next){
+userSchema.pre('save', function(next) {
     var user = this
 
+    // 비밀번호 암호화
     if (user.isModified('password')) {
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if (err) return next(err)
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if (err) return next(err)
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) {
+                return next(err)
+            }
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    return next(err)
+                }
+        
                 user.password = hash
                 next()
             })
         })
-    } else {
+    }
+    
+    else {
         next()
     }    
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb) {
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err),
-            cb(null, isMatch)
-    })
-}
-
-
-userSchema.methods.generateToken = function(cb) {
-    var user = this
-    var token = jwt.sign(user._id.toHexString(), 'secretToken')
-    user.token = token
-    user.save(function(err, user) {
-        if(err) return cb(err)
-        cb(null, user)
-    })
-}
-
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
+
+// userSchema.methods.comparePassword = function(plainPassword, cb) {
+//     bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+//         if (err) {
+//             return cb(err)
+//         }
+
+//         cb(null, isMatch)
+//     })
+// }
+// userSchema.methods.generateToken = function(cb) {
+//     var user = this
+//     var token = jwt.sign(user._id.toHexString(), 'secretToken')
+//     user.token = token
+//     user.save(function(err, user) {
+//         if(err) return cb(err)
+//         cb(null, user)
+//     })
+// }
