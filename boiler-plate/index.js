db584c3bbc6522275df7dd329b6ba37bf5e7f91a
MinJae Lee <wjdchs0129@gnu.ac.kr>
2021-12-06 21:06:26 +0900

    .

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,22 +3,50 @@ const app = express()
 const port = 5000
 const { User } = require('./models/User')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const config = require('./config/key')
 
-mongoose.connect("mongodb+srv://wjdchs0129:wjdchs012@boiler-plate.fkpxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect(config.mongoURI)
 .then(() => console.log('Well connected...'))
 .catch(err => console.log(err))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.get('/', (req, res) => res.send("민재 공부 테스트!!!"))
 
 app.post('/register', (req, res) => {
     const user = new User(req.body)
+
+
+
     user.save((err, doc) => {
         if (err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "해당 이메일을 가진 사용자가 없습니다."
+            }) 
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({loginSuccess: false, message: "올바른 비밀번호가 아닙니다."})
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err)
+                
+                res.cookie("test_cookie", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
         })
     })
 })
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { is } = require('type-is')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -29,6 +35,42 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+
+userSchema.pre('save', function(next){
+    var user = this
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }    
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if (err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
