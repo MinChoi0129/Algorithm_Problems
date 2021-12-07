17b80cf99ff8a35780ce62b54b696b49940fd968
MinJae Lee <wjdchs0129@gnu.ac.kr>
2021-12-07 20:19:21 +0900

    Update User.js

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -59,24 +59,23 @@ userSchema.pre('save', function(next) {
     }    
 })
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) {
+            return cb(err)
+        }
+        cb(null, isMatch)
+    })
+}
+userSchema.methods.generateToken = function(cb) {
+    var user = this
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
-
-// userSchema.methods.comparePassword = function(plainPassword, cb) {
-//     bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-//         if (err) {
-//             return cb(err)
-//         }
-
-//         cb(null, isMatch)
-//     })
-// }
-// userSchema.methods.generateToken = function(cb) {
-//     var user = this
-//     var token = jwt.sign(user._id.toHexString(), 'secretToken')
-//     user.token = token
-//     user.save(function(err, user) {
-//         if(err) return cb(err)
-//         cb(null, user)
-//     })
-// }
