4cdf1b329c6c05a17b1e1ae253a18e83dad71b5b
MinJae Lee <wjdchs0129@gnu.ac.kr>
2021-12-08 22:55:40 +0900

    User의 역할 분석 및 코드 수정

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -35,7 +35,6 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', function(next) {
     var user = this
-
     // 비밀번호 암호화
     if (user.isModified('password')) {
         bcrypt.genSalt(saltRounds, function(err, salt) {
@@ -47,7 +46,6 @@ userSchema.pre('save', function(next) {
                 if (err) {
                     return next(err)
                 }
-        
                 user.password = hash
                 next()
             })
@@ -67,6 +65,7 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
         cb(null, isMatch)
     })
 }
+
 userSchema.methods.generateToken = function(cb) {
     var user = this
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
@@ -76,6 +75,23 @@ userSchema.methods.generateToken = function(cb) {
         cb(null, user)
     })
 }
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this
+
+    // user._id + 'str' = token => token, str given => get user._id
+    //token decode
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        //  유저 아이디를 이용하여 유저 찾고
+        // 클라이언트 token === DB토큰 인지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if (err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
