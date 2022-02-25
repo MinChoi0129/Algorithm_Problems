4f4687b22ece795ec5a9d7888718292fb09799f0
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-02-25 21:56:16 +0900

    Update 16120.py
    class 활용하기 전,  먼저 작성한 문제 풀이 코드를 추가합니다.

--- Python_BOJ/16120.py
+++ Python_BOJ/16120.py
@@ -14,3 +14,13 @@ class PPAP:
         return True if check_list in PPAP.default_PPAP_strings else False
 
 print("PPAP" if PPAP(input()).isPPAP() else "NP")
+
+################### Non-Class Version #######################
+bryan_text = input()
+bryan_list = []
+for bryan_char in bryan_text:
+    bryan_list.append(bryan_char)
+    if bryan_list[-4:] == ['P', 'P', 'A', 'P']:
+        for _ in range(3): bryan_list.pop()
+
+print("PPAP" if bryan_list in [['P', 'P', 'A', 'P'], ['P']] else "NP")
