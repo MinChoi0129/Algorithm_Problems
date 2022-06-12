248c9aee745a584d23edcadef4f77b828f028efd
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-06-13 08:57:39 +0900

    .

---
+++ Python_ETC/instagramChecker.py
@@ -0,0 +1,48 @@
+import os
+followerFILE = open("C:/Users/leemi/Documents/GitHub/Coding-Example/Python_ETC/follower.txt", mode = "r",  encoding="utf-8")
+followingFILE = open("C:/Users/leemi/Documents/GitHub/Coding-Example/Python_ETC/tst.txt", mode = "r",  encoding="utf-8")
+follower, following, j = [], [], 0
+os.system("cls")
+
+for i in followerFILE:
+    if "님의 프로필 사진" in i:
+        follower.append(i[:-10])
+
+for i in followingFILE:
+    if "님의 프로필 사진" in i:
+        following.append(i[:-10])
+
+for human in follower:
+    if human not in following:
+        print(human)
+
+# len_of_longest_letter = 0
+# for i in follower:
+#     if len(i) > len_of_longest_letter:
+#         len_of_longest_letter = len(i)
+# for i in following:
+#     if len(i) > len_of_longest_letter:
+#         len_of_longest_letter = len(i)
+
+
+# print("팔로워 수: ", len(follower))
+# print("팔로잉 수: ", len(following))
+
+# print("\\n내가 팔로우 하지만 상대는 안하는 경우  : ")
+# for i in following:
+#     if i not in follower:
+#         print("%25s" %(i), end = " | ")
+#         j += 1
+#         if j == 4:
+#             print()
+#             j =  0
+
+# j = 0
+# print("\\n나는 팔로우 안하지만 상대가 하는 경우  : ")
+# for  i in follower:
+#     if i not in following:
+#         print("%25s" %(i), end = " | ")
+#         j += 1
+#         if j == 4:
+#             print()
+#             j =  0
