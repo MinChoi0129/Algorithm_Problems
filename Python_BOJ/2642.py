bdf53b3022f91cea68a55732c232e775cfd67773
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-07-27 09:44:01 +0900

    Update 2642.py

--- Python_BOJ/2642.py
+++ Python_BOJ/2642.py
@@ -1,62 +1,58 @@
-from copy import deepcopy as DC
-
-def rotated(array_2d):
+def rotated(array_2d) -> list: # 시계방향 90도 회전
     return [list(elem) for elem in zip(*array_2d[::-1])]
 
-def flipped(array_2d):
+def flipped(array_2d) -> list: # 윗면 아랫면 뒤집기
     return [line[::-1] for line in array_2d]
     
-def getCleanCubeFromInput() -> list:
-    board = []
-    for i in range(6):
+def cleanFigureFromInput() -> list: # 불필요한 000...000 라인을 제거
+    
+    # 가로 부분 제거
+    figure = []
+    for _ in range(6):
         line = input().split()
         if line != ['0'] * 6:
-            board.append(line)
+            figure.append(line)
 
-    rotated_board = []  
-    for line in rotated(board):
-        if line != ['0'] * len(line):
-            rotated_board.append(line)        
-    return rotated_board
+    # 세로부분 제거
+    rotated_figure = [line for line in rotated(figure) if line != ['0'] * len(line)] 
+    
+    return rotated_figure
 
-def getAnswers():
-    answers = [\
+def getAllFigures():
+    figures = [\
         [list('1000'), list('1111'), list('1000')], [list('0100'), list('1111'), list('1000')], [list('0010'), list('1111'), list('1000')], \
         [list('0001'), list('1111'), list('1000')], [list('0100'), list('1111'), list('0100')], [list('0010'), list('1111'), list('0100')], \
         [list('0011'), list('0110'), list('1100')], [list('0011'), list('1110'), list('1000')], [list('1100'), list('0111'), list('0100')], \
         [list('0100'), list('1110'), list('0011')], [list('00111'), list('11100')]
-    ] # 기본 전개도
+    ] # 기본 전개도 11종류.
     
-    for i in range(11): # 뒤집기
-        answers.append(flipped(answers[i]))
+    for i in range(11):# 11개에 대해 뒤집은 것 추가
+        figures.append(flipped(figures[i]))
 
-    for i in range(22): # 돌리기
-        arr90 = rotated(answers[i])
+    for i in range(22): # 22개에 대해서 90도씩 회전한 것 추가
+        arr90 = rotated(figures[i])
         arr180 = rotated(arr90)
         arr270 = rotated(arr180)
-        answers.append(arr90)
-        answers.append(arr180)
-        answers.append(arr270)
+        figures.append(arr90)
+        figures.append(arr180)
+        figures.append(arr270)
 
-    return answers
+    return figures
 
+figures = getAllFigures()
+my_figure = cleanFigureFromInput()
 
-answers = getAnswers()
-cube = getCleanCubeFromInput()
-new_cube = DC(cube) # 임시로 사용할 cube
+'''
+my_figure의 자연수 부분들을 모두 '1'로 바꾼 전개도 복사본(tmp_figure)이
+정육면체 전개도가 되긴 될 수 있는지 부터 파악
+'''
+tmp_figure = [['1' if my_figure[i][j] != '0' else '0' for j in range(len(my_figure[0]))] for i in range(len(my_figure))]
+is_exist = True if tmp_figure in figures else False # 정육면체가 될 수 있는지 여부.
 
-# 자연수를 모두 '1'로 바꿔 정육면체가 되긴 될 수 있는지 파악.
-for i in range(len(new_cube)):
-    for j in range(len(new_cube[0])):
-        if new_cube[i][j] != '0': new_cube[i][j] = '1'
-
-is_exist = True if new_cube in answers else False # 정육면체가 될 수 있는지 여부.
-
-if not is_exist: print(0)
+if not is_exist: print(0) # 정육면체 아님.
 else: # 마주보는 면 찾기
-    for x in range(len(cube)):
-        for y in range(len(cube[0])):
-            if cube[x][y] == '1': one_position = x, y
-
-    print(one_position) # '1'의 좌표
+    for x in range(len(my_figure)):
+        for y in range(len(my_figure[0])):
+            if my_figure[x][y] == '1': number_one_position = x, y # '1'의 좌표
     
+    print(my_figure[마주보는면x][마주보는면y])
