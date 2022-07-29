7c1bc23708ceac7fce8e4367b13556ed63896166
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-07-30 00:53:48 +0900

    update files

--- Python_BOJ/10875.py
+++ Python_BOJ/10875.py
@@ -2,14 +2,12 @@ def ccw(p1, p2, p3):
     s = (p1[0] * p2[1] + p2[0] * p3[1] + p3[0] * p1[1]) - (p1[1] * p2[0] + p2[1] * p3[0] + p3[1] * p1[0])
     return 1 if s > 0 else (0 if s == 0 else -1)
     
-
 class Line:
     def __init__(self, p1, p2, size, vector):
         self.start, self.end, self.size, self.vector = p1, p2, size, vector
         
     def isOutOfBoundary(self):
         global L
-        # 영역판단
         if   not (-L <= self.start[0] <= L): return [True, L + 1 +  self.end[0] ]
         elif not (-L <= self.start[1] <= L): return [True, L + 1 +  self.end[1] ]
         elif not (-L <=  self.end[0]  <= L): return [True, L + 1 - self.start[0]]
@@ -17,14 +15,23 @@ class Line:
         return [False, None]
     
     def isCrossingOtherLine(self, other_line):
-        # 충돌판단
         p1, p2, p3, p4 = self.start, self.end, other_line.start, other_line.end
         p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4)
         p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2)
         
-        if p1p2 == 0 and p3p4 == 0: # 일직선 상
-            result = p3 <= p2 and p1 <= p4 # 겹침여부
-            if result: return [True, p3[0] - p1[0]] if p1[1] == p2[1] else [True, p3[1] - p1[1]]
+        if p1p2 == 0 and p3p4 == 0: # 일직선 위에
+            result = (p3 <= p2 and p1 <= p4) # 겹침여부
+            if result:
+                option = 0 if p1[1] == p2[1] else 1
+                if self.vector == '+':
+                    if p1[option] < p4[option] < p2[option]:
+                        if p1[option] < p3[option]: return [True, p3[option] - p1[option]] # 선분 속 선분
+                        else: return [True, 0]
+                    else:
+                        if p3[option] < p1[option]: return [True, 0] # 선분 속 선분
+                        else: return [True, p3[option] - p1[option]]
+                else: return [True, p2[option] - p4[option]] if p1[option] < p4[option] < p2[option] else [True, 0]
+                    
         else: # 수직으로 교차
             result = p1p2 <= 0 and p3p4 <= 0
             if result:
@@ -35,7 +42,7 @@ class Line:
                     if self.vector == '+': return [True, p3[1] - p1[1]]
                     else: return [True, p2[1] - p3[1]]
         
-        return [False, None]
+        return [False, None] # 완벽한 선분.
         
     def isCrossingOrOutOfBoundary(self, other_line): # return [Boolean, timeUntilFalse]
         # 선 충돌판단, 후 영역판단
@@ -57,16 +64,16 @@ for dt, next_direction in [input().split() for _ in range(N)]:
 
     all_directions = ['U', 'L', 'D', 'R']
     direction = all_directions[(all_directions.index(direction) + (1 if next_direction == 'L' else -1)) % 4]
-    new_line = Line(p1, p2, int(dt), vector)
+    new_line = Line(p1, p2, int(dt), vector) # 오브젝트 생성.
     
     if len(lines) >= 3:
-        closest_line_distance = 3 * L
-        for old_line in lines[:-2]: # 최근 두개는 충돌가능성 없음.
-            result = new_line.isCrossingOrOutOfBoundary(old_line)
+        closest_line_distance = L * L # 적절히 큰 숫자
+        for old_line in lines[:-1]: # 최근 두개의 선분은 충돌가능성 없음.
+            result = new_line.isCrossingOrOutOfBoundary(old_line) # result = [가능여부, 불가능까지의 시간]
             if result[0]:
                 isSnakeDead = True
                 if result[1] < closest_line_distance: closest_line_distance = result[1]
-        if closest_line_distance < 3 * L: new_line.size = closest_line_distance
+        if closest_line_distance < L * L: new_line.size = closest_line_distance # 첫 충돌 판정시, 선분충돌까지의 길이를 업데이트.
         lines.append(new_line)
     else:
         is_out_of_boundary = new_line.isOutOfBoundary()
@@ -93,25 +100,23 @@ if not isSnakeDead:
     new_line = Line(p1, p2, int(dt), vector)
     
     if len(lines) >= 3:
-        closest_line_distance = 3 * L
-        for old_line in lines[:-2]: # 최근 두개는 충돌가능성 없음.
+        closest_line_distance = L * L
+        for old_line in lines[:-1]: # 최근 두개는 충돌가능성 없음.
             result = new_line.isCrossingOrOutOfBoundary(old_line)
             if result[0]:
                 isSnakeDead = True
                 if result[1] < closest_line_distance: closest_line_distance = result[1]
-        if closest_line_distance < 3 * L: new_line.size = closest_line_distance
+        if closest_line_distance < L * L: new_line.size = closest_line_distance
         lines.append(new_line)
     else:
         is_out_of_boundary = new_line.isOutOfBoundary()
         if is_out_of_boundary[0]:
             new_line.size = is_out_of_boundary[1]
-            isSnakeDead = True
             lines.append(new_line)
+            isSnakeDead = True
         else:
             lines.append(new_line)
-        
-        
-    
+
 answer = 0
 for line in lines: answer += line.size
 print(answer if N != 0 else L + 1)
--- Python_BOJ/test_runner.py
+++ Python_BOJ/test_runner.py
@@ -1,24 +1,18 @@
-main = __import__("1195")
+# main = __import__("10875")
 
 def test():
-    for i in range(3):
-        for j in range(10):
-            if i == j == 0: continue
-            if i*j >= 16: break
-            print("* TEST " + str(i) + str(j))
-            problem_f = open("E:/Downloads/tests/tests/kickdown/tests/" + str(i) + str(j), 'r')
-            answer_f = open("E:/Downloads/tests/tests/kickdown/tests/" + str(i) + str(j) + '.a', 'r')
-            
-            inputs = problem_f.readlines()
-            answer = int(answer_f.readline().rstrip())
-            
-            line_a, line_b = list(inputs[0].rstrip()), list(inputs[1].rstrip())
-            
-            my_answer = main.main((line_a, line_b))
-            if my_answer != answer: print("FAILED", my_answer, answer)
-            else: print("OK")
-            
-            problem_f.close()
-            answer_f.close()
-
+    texts = open("E:/Downloads/tests/abc/abcd.txt", 'r')
+    
+    input_txt = ""
+    while True:
+        line = texts.readline().rstrip()
+        if line == "EOF": break
+        if line.startswith('TestCase'):
+            print(line)
+        input_txt += line
+        if not line:
+            answer = int(texts.readline())
+        
+    
+    texts.close()
 test()
