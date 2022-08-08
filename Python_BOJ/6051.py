f2c7c6d0357386f25369863d57426d1d7801a0b7
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-08-08 16:14:39 +0900

    Update 6051.py

--- Python_BOJ/6051.py
+++ Python_BOJ/6051.py
@@ -1,12 +1,11 @@
 import sys, copy
 input = lambda : sys.stdin.readline().rstrip()
-DC = lambda : copy.deepcopy()
 logs, now = [[]], []
 for _ in range(int(input())):
     query = input().split()
     if len(query) == 1: now.pop()
     elif query[0] == 'a': now.append(int(query[1]))
-    else: now = DC(logs[int(query[1])-1])
+    else: now = copy.deepcopy(logs[int(query[1])-1])
     try: print(now[-1])
     except: print(-1)
-    finally: logs.append(DC(now))
+    finally: logs.append(copy.deepcopy(now))
