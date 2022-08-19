0f632e4b9a2f076db33ee4462e303e858cd702e8
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-08-19 15:16:01 +0900

    Update 1407.py

--- Python_BOJ/1407.py
+++ Python_BOJ/1407.py
@@ -3,6 +3,6 @@ def S(n):
     if not n: return 0
     elif n == 1: return 1
     elif n % 2 == 0: return n//2 + 2*S(n//2)
-    else: return n//2 + 1 + 2*S((n-1)//2)
+    else: return (n//2 + 1) + 2*S((n-1)//2)
 a, b = map(int, input().split())
 print(S(b)-S(a-1))
