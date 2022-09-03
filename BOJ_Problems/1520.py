18bb608b77d3cae6e81605802f395ae52952ec72
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-09-03 23:00:06 +0900

    Update 1520.py

--- BOJ_Problems/1520.py
+++ BOJ_Problems/1520.py
@@ -3,7 +3,7 @@ sys.setrecursionlimit(1000000)
 input = lambda : sys.stdin.readline().rstrip()
 def dfs(x, y):
     if [x, y] == [m-1, n-1]: return 1
-    if dp[x][y] != 0: return dp[x][y]
+    if dp[x][y] != -1: return dp[x][y]
     route = 0
     for dx, dy in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
         if 0 <= x+dx < m and 0 <= y+dy < n and board[x+dx][y+dy] < board[x][y]: route += dfs(x+dx, y+dy)
@@ -12,5 +12,5 @@ def dfs(x, y):
 
 m, n = map(int, input().split())
 board = [[*map(int, input().split())] for _ in range(m)]
-dp = [[0] * n for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
 print(dfs(0, 0))
