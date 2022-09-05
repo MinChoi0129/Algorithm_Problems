59f1af18e2ad18ad060ca82fff4881d497251ff8
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-09-05 13:26:47 +0900

    Update 9735.py

--- BOJ_Problems/9735.py
+++ BOJ_Problems/9735.py
@@ -1,19 +1,18 @@
 for _ in range(int(input())):
     a, b, c, d = map(int, input().split())
-    xs = []
+    xs = set()
+    N = None
     for x in range(-1000000, 1000001):
         if a*(x**3)+b*(x**2)+c*x+d == 0:
-            xs.append(x); break
-    N = xs[0]
+            N = x; break
+ 
     A, B, C = a, a*N+b, a*(N**2)+b*N+c
     D = B**2 - 4*A*C
-    if D == 0:
-        x = (-B) / (2*A)
-        if x != N: xs.append(x)
+    if D == 0: xs.add((-B) / (2*A))
     elif D > 0:
         x1, x2 = ((-B) + D ** 0.5) / (2*A), ((-B) - D ** 0.5) / (2*A)
-        if x1 != N: xs.append(x1)
-        if x2 != N: xs.append(x2)
-
+        xs.add(x1)
+        xs.add(x2)
+    xs.add(N)
     for x in sorted(xs): print("%.4f" % x, end = " ")
     print()
