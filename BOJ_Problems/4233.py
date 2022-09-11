f0a47409594d43963bbeb4c83565b5e41fa89cd8
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-09-11 20:17:17 +0900

    Update 4233.py

--- BOJ_Problems/4233.py
+++ BOJ_Problems/4233.py
@@ -1,13 +1,7 @@
-def caulcateConquerPowerWithMod(base, exp, mod):
-    if exp == 0: return 1
-    elif exp == 1: return base
-
-    if exp % 2 == 0:
-        value = caulcateConquerPowerWithMod(base, exp // 2, mod)
-        return ((value % mod) ** 2) % mod
-    else:
-        value = caulcateConquerPowerWithMod(base, (exp-1) // 2, mod)
-        return base * (((value % mod) ** 2) % mod)
+def calculateConquerPowerWithMod(base, exp, mod):
+    if exp <= 1: return base ** exp
+    elif exp % 2 == 0: return ((calculateConquerPowerWithMod(base, exp // 2, mod) % mod) ** 2) % mod
+    else: return base * (((calculateConquerPowerWithMod(base, (exp-1) // 2, mod) % mod) ** 2) % mod)
 
 def isPrime(num):
     for i in range(2, int(num ** 0.5) + 1):
@@ -18,4 +12,4 @@ while True:
     p, a = map(int, input().split())
     if p == a == 0: break
     if isPrime(p): print("no")
-    else: print("yes" if caulcateConquerPowerWithMod(base = a, exp = p, mod = p) % p == a % p else "no")
+    else: print("yes" if calculateConquerPowerWithMod(a, p, p) % p == a else "no")
