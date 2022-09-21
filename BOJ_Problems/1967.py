e04ceb25103cc3eb05f0467f94b41f6c2c3f6b6e
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-09-21 23:41:00 +0900

    Update 1967.py

--- BOJ_Problems/1967.py
+++ BOJ_Problems/1967.py
@@ -1,17 +1,13 @@
-import sys
+import sys, random
 input = lambda : sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10000000)
 
-def getDistanceBetweenTwoNodes(start, end, visited):
-    if start == end: return [True, 0, end]
-    visited[start] = True
-    for node, distance in connections[start]:
-        if not visited[node]:
-            visited[node] = True
-            result = getDistanceBetweenTwoNodes(node, end, visited)
-            if result[0]:
-                return [True, distance + result[1], end]
+def dfs(node, distance):
+    for new_node, new_distance in connections[node]:
+        if visited[new_node] == -1:
+            visited[new_node] = distance + new_distance
+            dfs(new_node, distance + new_distance)
 
-    return [False, None, end]
 n = int(input())
 connections = {i+1: [] for i in range(n)}
 for _ in range(n-1):
@@ -19,18 +15,16 @@ for _ in range(n-1):
     connections[node1].append((node2, distance))
     connections[node2].append((node1, distance))
 
-diameter, target_node = 0, None
-for node_num in range(2, n + 1):
-    visited = [False] * (n + 1); visited[0] = None
-    result = getDistanceBetweenTwoNodes(1, node_num, visited)
-    if result[1] > diameter:
-        diameter = result[1]
-        target_node = result[2]
+# Trial 1
+start_node = random.randint(1, n)
+visited = [-1] * (n + 1)
+visited[start_node] = 0
+dfs(start_node, 0)
 
-for node_num in range(1, n + 1):
-    visited = [False] * (n + 1); visited[0] = None
-    result = getDistanceBetweenTwoNodes(target_node, node_num, visited)
-    if result[1] > diameter:
-        diameter = result[1]
+# Trial 2
+start_node = visited.index(max(visited))
+visited = [-1] * (n + 1)
+visited[start_node] = 0
+dfs(start_node, 0)
 
-print(diameter)
+print(max(visited))
