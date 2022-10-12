e920abbf105dcd79d3afd29f9a8c80100e80d0de
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-10-12 22:22:49 +0900

    Update 11778.py

--- BOJ_Problems/11778.py
+++ BOJ_Problems/11778.py
@@ -1,15 +1,9 @@
 class Matrix:
-    def __init__(self, matrix_array):
-        self.matrix = matrix_array
-        self.base_matrix = matrix_array
-    
+    def __init__(self, matrix_array): self.matrix = matrix_array; self.base_matrix = matrix_array
     def __mul__(self, other):
         a, b, c, d = self.matrix[0][0], self.matrix[0][1], self.matrix[1][0], self.matrix[1][1]
         e, f, g, h = other.matrix[0][0], other.matrix[0][1], other.matrix[1][0], other.matrix[1][1]
-        return Matrix([\
-            [   ((a*e) % P + (b*g) % P) % P,    ((a*f) % P + (b*h) % P) % P     ],
-            [   ((c*e) % P + (d*g) % P) % P,    ((c*f) % P + (d*h) % P) % P     ]
-        ])
+        return Matrix([[((a*e) % P + (b*g) % P) % P, ((a*f) % P + (b*h) % P) % P], [((c*e) % P + (d*g) % P) % P, ((c*f) % P + (d*h) % P) % P]])
     def __pow__(self, k):
         if k == 1: return Matrix(self.base_matrix)
         c = self ** (k//2)
