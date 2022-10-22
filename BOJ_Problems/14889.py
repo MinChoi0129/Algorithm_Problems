cc5fd8e3ca10a6bccc0b84ea78212d52c906927c
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-10-22 16:36:44 +0900

    solved

--- BOJ_Problems/14889.py
+++ BOJ_Problems/14889.py
@@ -1,27 +1,25 @@
-from math import factorial
 from itertools import combinations as C
-n = int(input())
-table = [[*map(int, input().split())] for _ in range(n)]
 
-all_cases = set()
-cut = (factorial(n) / (factorial(n//2)**2)) // 2
-a = set(range(n))
-count = 0
-for case in C(range(n), n // 2):
-    if count == cut: break
-    b = set(case)
-    count += 1
-    c, d = list(b), list(a-b)
-    for i in range(n // 2):
-        i %= n//2
-        print("TEAM-A")
-        print(c[i], c[i-1], end = " ")
-        print()
-    for i in range(n // 2):
-        i %= n//2
-        print("TEAM-B")
-        print(d[i], d[i-1], end = " ")
-        print()
-    
-    
-    
+n = int(input())
+board = [[*map(int, input().split())] for _ in range(n)]
+
+min_gap = int(1e9)
+players = set(range(n))
+
+start_cases = C(players, n//2)
+for start_case in start_cases:
+    link_case = players - set(start_case)
+
+    start_power, link_power = 0, 0
+    start_case, link_case = tuple(start_case), tuple(link_case) # 인덱싱을 위한 단순한 형변환.
+    for i in range(n//2 - 1):
+        for j in range(i+1, n//2):
+            start_1, start_2 = start_case[i], start_case[j]
+            link_1, link_2 = link_case[i], link_case[j]
+
+            start_power += board[start_1][start_2] + board[start_2][start_1]
+            link_power += board[link_1][link_2] + board[link_2][link_1]
+
+    min_gap = min(min_gap, abs(link_power - start_power))
+
+print(min_gap)
