357bfa22692525f58bca8fa4cc0f8b9bf52e37f9
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-02-20 01:15:10 +0900

    .

--- BOJ_Problems/1353.py
+++ BOJ_Problems/1353.py
@@ -1,15 +1,25 @@
-from math import pow as new_pow
+"""c만 통과됨."""
+# import math, decimal
+# decimal.getcontext().prec = 100
+# e = decimal.Decimal('2.7182818459045')
 
-def find_n(e, s, p):
-    if s == p: return 1
-    if new_pow(e, s / e) < p: return -1
+# def solution():
+#     s, p = map(decimal.Decimal, input().split())
+#     if s == p:
+#         return 1
 
-    old = -1;
-    for x in range(2, s):
-        new = new_pow(s / x, x);
-        if old > new: return -1
-        if new >= p: return x
-        old = new;
+#     if decimal.Decimal(math.pow(e, s / e)) < p:
+#         return -1
 
-e, s, p = 2.7182818459045, *map(int, input().split())
-print(find_n(e, s, p))
+#     prv = decimal.Decimal('-1');
+#     i = 2;
+#     while True:
+#         cur = decimal.Decimal(math.pow(s / i, i))
+#         if (prv > cur):
+#             return -1
+#         if (cur >= p):
+#             return i
+#         prv = cur
+#         i += 1
+
+# print(solution())
