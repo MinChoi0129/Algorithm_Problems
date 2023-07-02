40741339e1e970e7bcd768e0dd7c837b65a12876
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-07-02 23:03:42 +0900

    Create 13414.py

---
+++ BOJ_Problems/13414.py
@@ -0,0 +1,10 @@
+server = {}
+
+k, l = map(int, input().split())
+
+for _ in range(l):
+    student_id = input()
+    if student_id not in server: server[student_id] = True
+    else: server[student_id] = server.pop(student_id)
+
+print(*[*server.keys()][:k], sep="\n")
