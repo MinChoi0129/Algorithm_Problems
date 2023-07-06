d1a5189984217497220768eed085aff5556bb8d5
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-07-07 01:19:51 +0900

    Update 2252.py

--- BOJ_Problems/2252.py
+++ BOJ_Problems/2252.py
@@ -1,21 +1,25 @@
-def dfs(node_num):
-    if not visited[node_num]:
-        visited[node_num] = True
-        for node in connections[node_num]:
-            dfs(node)
-        order.append(node_num)
+from collections import deque
 
 n, m = map(int, input().split())
-connections = {i: [] for i in range(1, n+1)}
-
-visited = [None] + [False] * n
-order = []
+connections = {student_num: [] for student_num in range(1, n+1)}
+indegrees = {student_num: 0 for student_num in range(1, n+1)}
 
 for _ in range(m):
     a, b = map(int, input().split())
-    connections[a].append(b)
+    connections[a].append(b) # connection 정보 추가
+    indegrees[b] += 1
 
-for i in range(1, n+1):
-    dfs(i)
+Q = deque()
 
-print(*reversed(order))
+for student_num in range(1, n+1):
+    if not indegrees[student_num]:
+        Q.append(student_num)
+
+while Q:
+    student_num = Q.popleft()
+    print(student_num, end = " ") 
+    for adj_student_num in connections[student_num]:
+        indegrees[adj_student_num] -= 1
+        if not indegrees[adj_student_num]:
+            Q.append(adj_student_num)
+    
