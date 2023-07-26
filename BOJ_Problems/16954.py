5cdf72a3ca9792be2935d634d3b96a756617f135
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-07-26 15:48:10 +0900

    Update 16954.py

--- BOJ_Problems/16954.py
+++ BOJ_Problems/16954.py
@@ -1,67 +1,51 @@
 from collections import deque
 
-
-def moveCharacter(board, x, y, direction):
-    """
-    direction
-    0: 제자리, 1: N, 2: NE, 3: E, ... , 7: W, 8: NW
-    """
-    dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
-    dy = [0, 0, 1, 1, 1, 0, -1, -1, -1]
-    new_x, new_y = x + dx[direction], y + dy[direction]
-    if 0 <= new_x < 8 and 0 <= new_y < 8:
-        if board[new_x][new_y] == "." or board[new_x][new_y] == "M":
-            board[x][y] = "."
-            board[new_x][new_y] = "M"
-            return board, new_x, new_y
-        else:
-            raise Exception("Wall Exists")
-    else:
-        raise Exception("Out of Boundary")
+dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, 0, 1, 1, 1, 0, -1, -1, -1]
 
 
-def moveWalls(board, positions):
-    wall_positions = []
-    for x, y in positions:
-        new_x, new_y = x + 1, y
-        if 0 <= new_x < 8 and 0 <= new_y < 8:
-            if board[new_x][new_y] == "M":
-                raise Exception("Character Exists")
-            else:  # 벽 이동
-                board[x][y] = "."
-                board[new_x][new_y] = "#"
-                wall_positions.append((new_x, new_y))
-        else:  # 벽 사라짐
-            pass
-    return board, wall_positions
-
-
-def BFS(Q):
+def BFS(Q: deque):
     while Q:
-        new_board, wall_positions, new_x, new_y = Q.popleft()
+        current_character_x, current_character_y = Q.popleft()
+
+        # 캐릭터 이동
+        new_character_positions = set()
         for direction in range(9):
-            try:
-                new_board, new_x, new_y = moveCharacter(
-                    new_board, new_x, new_y, direction
-                )
-                new_board, new_wall_positions = moveWalls(new_board, wall_positions)
-                if new_x == 0 and new_y == 7:
-                    return 1
-                Q.append((new_board, new_wall_positions, new_x, new_y))
-            except:
-                continue
+            new_character_x, new_character_y = (
+                current_character_x + dx[direction],
+                current_character_y + dy[direction],
+            )
+            if (new_character_x, new_character_y) == (0, 7):
+                return 1
+            elif (
+                0 <= new_character_x < 8
+                and 0 <= new_character_y < 8
+                and (new_character_x, new_character_y) not in wall_positions
+                and (new_character_x, new_character_y) not in visited;
+            ):
+                new_character_positions.add((new_character_x, new_character_y))
+
+        # 벽 이동
+        new_wall_positions = set()
+        for wall_x, wall_y in wall_positions:
+            new_wall_x, new_wall_y = wall_x + 1, wall_y
+            if 0 <= new_wall_x < 8 and 0 <= new_wall_y < 8:
+                new_wall_positions.add((new_wall_x, new_wall_y))
+
+        # 캐릭터의 9방향 이동 이후에 벽이 움직인다 할지라도 충돌하지 않는 좌표민
+        Q.extend([*(new_character_positions - new_wall_positions)])
+
     return 0
 
 
-board, wall_positions = [], []
+board, wall_positions = [], set()
 for x in range(8):
     line = list(input())
     for y in range(8):
         if line[y] == "#":
-            wall_positions.append((x, y))
+            wall_positions.add((x, y))
     board.append(line)
 
-x, y = 7, 0
-board[x][y] = "M"
-Q = deque([(board, wall_positions, x, y)])
+start_character_x, start_character_y = 7, 0
+Q = deque([(start_character_x, start_character_y)])
 print(BFS(Q))
